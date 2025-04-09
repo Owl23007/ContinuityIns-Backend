ef973f59aa0471cab31c6e0d1237df2373c2df24
Oii Woof <w15072362290@163.com>
2025-04-09 23:30:55 +0800

    feat(oss): 重构 OSS工具类并添加 STS Token 支持
    - 重构 AliOssUtil 类，添加获取 STS Token 的功能
    - 修改 UserController 和 UserService 接口，支持新的 OSS 策略获取方法- 移除不必要的视频相关代码，简化项目结构
    - 优化 AI 聊天功能，支持模型选择和错误处理

--- src/main/resources/MySQL.sql
+++ src/main/resources/MySQL.sql
@@ -132,4 +132,3 @@ CREATE TABLE tag_articles (
                              FOREIGN KEY (tag_id) REFERENCES tags(tag_id) ON DELETE CASCADE,
                              FOREIGN KEY (article_id) REFERENCES articles(article_id) ON DELETE CASCADE
 );
-
